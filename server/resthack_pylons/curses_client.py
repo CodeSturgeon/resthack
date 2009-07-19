a0615f0dd798c334ad97ef3c4068fbe3c7be1ee7
CodeSturgeon <github@nickdafish.com>
2009-07-19 10:01:46 +0000

    added logging support to curses client
    git-svn-id: http://codesturgeon.svn.beanstalkapp.com/resthack@20 80554853-e439-4f06-b961-9753fd219b06

--- server/resthack_pylons/curses_client.py
+++ server/resthack_pylons/curses_client.py
@@ -3,27 +3,48 @@ import curses
 import simplejson
 import urllib2, urllib
 
-screen = curses.initscr()
-curses.noecho()
-screen.clear()
-#w_width = curses.COLS-2
-#w_height = curses.LINES-2
-w_width = 40
-w_height = 20
+import logging
+from inspect import getouterframes, currentframe
 
-main_w = curses.newwin(w_height+2,w_width+2,0,0)
-main_w.box()
-main_w.overlay(screen)
-screen.refresh()
-main_w.refresh()
+class QueueingHandler(logging.Handler):
+    queue = []
+    def emit(self,record):
+        self.queue.append(record)
+    def flush(self):
+        flush = map(self.format,self.queue)
+        self.queue = []
+        return flush
+
+def config_log():
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    qh = QueueingHandler()
+    qh.setLevel(logging.DEBUG)
+    qh.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+    root.addHandler(qh)
+    logging.basicConfig()
+    return qh
+
+def get_log():
+    name_mod = __name__
+    name_caller = getouterframes(currentframe())[1][3]
+    if name_mod != '__main__':
+        name = '.'.join([name_mod, name_caller])
+    else:
+        name = name_caller
+    return logging.getLogger(name)
 
 def move(direction):
     params = urllib.urlencode(dict(move=direction))
     url = 'http://localhost:5421/space/move'
     resp = urllib2.urlopen(url, params).read()
-    return move
+    return resp
 
 def show_map(m_x, m_y):
+    log = get_log()
+    log.error('hello world')
+    log.error('hello world2')
+    log.error('hello world3')
     url = 'http://localhost:5421/space/%d,%d-%d,%d'%(
             m_x,m_y,m_x+w_width,m_y+w_height)
     resp = urllib2.urlopen(url).read()
@@ -50,12 +71,34 @@ def show_map(m_x, m_y):
                 c = '#'
             elif x == p['x'] and y == p['y']:
                 c = 'P'
-            main_w.addch(1+(y-m_y),1+(x-m_x),c)
+            w_map.addch(1+(y-m_y),1+(x-m_x),c)
 
-    main_w.refresh()
+    w_map.refresh()
 
+
+screen = curses.initscr()
+curses.noecho()
+screen.clear()
+#curses.curs_set(0)
+#w_width = curses.COLS-2
+#w_height = curses.LINES-2
+w_width = 40
+w_height = 20
+
+w_map = curses.newwin(w_height+2,w_width+2,0,0)
+w_map.box()
+w_map.overlay(screen)
+
+w_log = curses.newwin(w_height+2,w_width+2,w_height+2,0)
+w_log.box()
+w_log.overlay(screen)
+
+screen.refresh()
+
+qh = config_log()
 c = x = y = 0
 moves = {'n':(0,-1), 'e':(1,0), 's':(0,1), 'w':(-1,0)}
+
 while c != 'q':
     show_map(x,y)
     c = chr(screen.getch())
@@ -66,4 +109,12 @@ while c != 'q':
         x = x+moves[c][0]
         y = y+moves[c][1]
 
+    log_messages = qh.flush()
+    w_log.clear()
+    w_log.box()
+    for l_y in range(1,len(log_messages)+1):
+        w_log.addstr(l_y,1,log_messages[l_y-1])
+    w_log.refresh()
+
 curses.endwin()
+curses.echo()
