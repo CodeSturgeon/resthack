1dc9905d142ad90751337680d59f30efcd39943c
CodeSturgeon <github@nickdafish.com>
2009-07-19 10:01:49 +0000

    tweaking the logging in curese client
    git-svn-id: http://codesturgeon.svn.beanstalkapp.com/resthack@21 80554853-e439-4f06-b961-9753fd219b06

--- server/resthack_pylons/curses_client.py
+++ server/resthack_pylons/curses_client.py
@@ -6,26 +6,27 @@ import urllib2, urllib
 import logging
 from inspect import getouterframes, currentframe
 
-class QueueingHandler(logging.Handler):
-    queue = []
-    def emit(self,record):
-        self.queue.append(record)
-    def flush(self):
-        flush = map(self.format,self.queue)
-        self.queue = []
-        return flush
+w_log = ''
+
+class WindowHandler(logging.Handler):
+    offset = 1
+    def emit(self, record):
+        w_log.addstr(self.offset, 1, self.format(record))
+        self.offset += 1
+        w_log.refresh()
 
 def config_log():
     root = logging.getLogger()
     root.setLevel(logging.DEBUG)
-    qh = QueueingHandler()
+    #qh = QueueingHandler()
+    qh = WindowHandler()
     qh.setLevel(logging.DEBUG)
     qh.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
     root.addHandler(qh)
     logging.basicConfig()
     return qh
 
-def get_log():
+def get_log(level=logging.DEBUG):
     name_mod = __name__
     name_caller = getouterframes(currentframe())[1][3]
     if name_mod != '__main__':
@@ -35,6 +36,8 @@ def get_log():
     return logging.getLogger(name)
 
 def move(direction):
+    log = get_log()
+    log.info('moving %s'%direction)
     params = urllib.urlencode(dict(move=direction))
     url = 'http://localhost:5421/space/move'
     resp = urllib2.urlopen(url, params).read()
@@ -42,15 +45,15 @@ def move(direction):
 
 def show_map(m_x, m_y):
     log = get_log()
-    log.error('hello world')
-    log.error('hello world2')
-    log.error('hello world3')
+    log.debug('getting space')
     url = 'http://localhost:5421/space/%d,%d-%d,%d'%(
             m_x,m_y,m_x+w_width,m_y+w_height)
     resp = urllib2.urlopen(url).read()
 
+    log.debug('parsing space')
     things = simplejson.loads(resp)
 
+    log.debug('building walls')
     walls1 = []
     walls2 = []
     p = {'x':0,'y':0}
@@ -62,6 +65,7 @@ def show_map(m_x, m_y):
         elif thing['type'] == 'wall2':
             walls2.append((thing['x'],thing['y']))
 
+    log.debug('updating viewport')
     for y in range(m_y,m_y+w_height):
         for x in range(m_x,m_x+w_width):
             c = ' '
@@ -74,47 +78,47 @@ def show_map(m_x, m_y):
             w_map.addch(1+(y-m_y),1+(x-m_x),c)
 
     w_map.refresh()
+    log.debug('done')
 
 
 screen = curses.initscr()
 curses.noecho()
 screen.clear()
 #curses.curs_set(0)
-#w_width = curses.COLS-2
-#w_height = curses.LINES-2
-w_width = 40
-w_height = 20
+log_lines = 10
+w_width = curses.COLS-2
+w_height = curses.LINES-4-log_lines
 
 w_map = curses.newwin(w_height+2,w_width+2,0,0)
 w_map.box()
 w_map.overlay(screen)
 
-w_log = curses.newwin(w_height+2,w_width+2,w_height+2,0)
+w_log = curses.newwin(log_lines+2,w_width+2,w_height+2,0)
 w_log.box()
 w_log.overlay(screen)
 
 screen.refresh()
 
 qh = config_log()
-c = x = y = 0
+log = logging.getLogger()
+x = y = 0
+c = ''
 moves = {'n':(0,-1), 'e':(1,0), 's':(0,1), 'w':(-1,0)}
+show_map(x,y)
 
 while c != 'q':
-    show_map(x,y)
+    w_log.clear()
+    w_log.box()
+    qh.offset = 1
     c = chr(screen.getch())
+    log.debug('key: %s'%c)
     if c in moves.keys():
         move(c)
     elif c.lower() in moves.keys():
         c = c.lower()
-        x = x+moves[c][0]
-        y = y+moves[c][1]
-
-    log_messages = qh.flush()
-    w_log.clear()
-    w_log.box()
-    for l_y in range(1,len(log_messages)+1):
-        w_log.addstr(l_y,1,log_messages[l_y-1])
-    w_log.refresh()
+        x = x+(moves[c][0]*5)
+        y = y+(moves[c][1]*5)
+    show_map(x,y)
 
 curses.endwin()
 curses.echo()
