d4454f3164626e16b83c96d962c4ac1025857737
johnc303 <castle.johnathan@googlemail.com>
2010-10-20 12:45:49 +0000

    Adding JSON version, prior to refactoring into JSONP
    git-svn-id: http://codesturgeon.svn.beanstalkapp.com/resthack@94 80554853-e439-4f06-b961-9753fd219b06

---
+++ client/v0.2/js/eventmanager.js
@@ -0,0 +1,37 @@
+var EM =
+{
+	strName:"The event manager",
+	_arrRegisteredObjects: [],
+
+	register: function (_objWhatObject)
+	{
+		this._arrRegisteredObjects[this._arrRegisteredObjects.length] = _objWhatObject;
+		this.debug("EM.register(_objWhatObject:)", 1, _objWhatObject);
+	},
+
+	trigger: function(_strEventName, _mixParam)
+	{
+		var _booBubbleUp = true;
+		// Attempt to fire event on registered objects
+		for (var i=0; i<this._arrRegisteredObjects.length; i++)
+		{
+			this.fireIfHandled(this._arrRegisteredObjects[i], _strEventName, _mixParam);
+		}
+	},
+
+	fireIfHandled: function(_obj, _strEventName, _mixParam)
+	{
+		if (_obj['handleEvent_'+_strEventName])
+		{
+			_obj['handleEvent_'+_strEventName](_mixParam);
+		}
+	},
+
+	debug : function (strMessage, intPriority, objCallerObject, booCalleeChain)
+	{
+		if (DEBUG)
+		{
+			DEBUG.lert(strMessage, intPriority, objCallerObject, booCalleeChain);
+		}
+	}
+}
